bb20ed222cf52912ab2c3ab9ab3c51b58aa6213b
sterlinr08-dte <sterlinr08@gmail.com>
2026-09-12 19:23:19 -0400

    Bust social extension cache after Facebook loading fix

--- crm-marketing-consent.js
+++ crm-marketing-consent.js
@@ -4,7 +4,7 @@
   if(window.__bcCrmExtensionsLoader)return;
   window.__bcCrmExtensionsLoader=true;
 
-  var V='20260912h';
+  var V='20260912i';
   var legacy=document.createElement('script');
   legacy.src='crm-marketing-consent-legacy.js?v='+V;
   legacy.onload=function(){
