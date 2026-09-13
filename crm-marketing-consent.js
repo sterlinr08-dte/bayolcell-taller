eeb5c3dde6b7f0cfea8e94276408630a59b9b336
sterlinr08-dte <sterlinr08@gmail.com>
2026-09-12 20:11:11 -0400

    fix(crm): invalidate cache for audited Facebook chat fixes

--- crm-marketing-consent.js
+++ crm-marketing-consent.js
@@ -4,7 +4,7 @@
   if(window.__bcCrmExtensionsLoader)return;
   window.__bcCrmExtensionsLoader=true;
 
-  var V='20260912l';
+  var V='20260913a';
   var legacy=document.createElement('script');
   legacy.src='crm-marketing-consent-legacy.js?v='+V;
   legacy.onload=function(){
