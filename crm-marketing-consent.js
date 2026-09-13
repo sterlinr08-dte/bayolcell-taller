0a85fc731404dc5570f6f74ec38983ba7a9d5c15
sterlinr08-dte <sterlinr08@gmail.com>
2026-09-12 20:43:49 -0400

    fix(crm): remove Safari keyboard viewport double offset

--- crm-marketing-consent.js
+++ crm-marketing-consent.js
@@ -4,7 +4,7 @@
   if(window.__bcCrmExtensionsLoader)return;
   window.__bcCrmExtensionsLoader=true;
 
-  var V='20260913-facebook-keyboard';
+  var V='20260913-facebook-keyboard2';
   var legacy=document.createElement('script');
   legacy.src='crm-marketing-consent-legacy.js?v='+V;
   legacy.onload=function(){
