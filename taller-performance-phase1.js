717dfd11889ab01d4e5d15b7cf54b83995a7b13e
sterlinr08-dte <sterlinr08@gmail.com>
2026-09-16 22:24:00 -0400

    rollback(crm): retirar estabilidad social regresiva

--- taller-performance-phase1.js
+++ taller-performance-phase1.js
@@ -9,7 +9,7 @@
   if(window.__bcPerfPhase1) return;
   window.__bcPerfPhase1 = true;
 
-  const VERSION = '20260916.3';
+  const VERSION = '20260916.2';
   const BACKUP_POLL_MS = 5 * 60 * 1000;
   const CORE_TABLES = new Set([
     'ordenes_reparacion','clientes','equipos','piezas_inventario','activos_taller',
@@ -136,25 +136,12 @@
     } catch(_e) {}
   }
 
-  function loadFacebookRefreshStability(){
-    try {
-      if(window.__bcFbRefreshStabilityRequested) return;
-      window.__bcFbRefreshStabilityRequested = true;
-      const s = document.createElement('script');
-      s.async = true;
-      s.src = 'crm-facebook-refresh-stability.js?v=20260916-fb1';
-      s.onerror = function(){ window.__bcFbRefreshStabilityRequested = false; };
-      document.head.appendChild(s);
-    } catch(_e) {}
-  }
-
   function install(){
     installLoadAllMetrics();
     installRealtimeAllowlist();
     installBackupPoller();
     installResumeSafety();
     loadWhatsAppPagination();
-    loadFacebookRefreshStability();
   }
 
   install();
@@ -172,8 +159,7 @@
         ignoredByTable: Object.assign({}, state.realtime.ignoredByTable)
       },
       pollerInstalled: !!state.pollerInstalled,
-      whatsappPaging: !!window.__bcWaPaginationInstalled,
-      facebookRefreshStability: !!window.__bcFbRefreshStability
+      whatsappPaging: !!window.__bcWaPaginationInstalled
     };
   };
 })();
